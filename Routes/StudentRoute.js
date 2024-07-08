30d52ff5a4b2d86c7877d3ecbee4aec00674df4b
Chirag Singh <Chirag.Singh@365labs.com>
2024-07-08 22:07:40 +0530

    add student promot route

--- Routes/StudentRoute.js
+++ Routes/StudentRoute.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { GetStudent, SearchStudent,SearchStudentByClass, PostStudent, PatchStudent, DeleteStudent, AddAttendance } = require('../Controllers/StudentsController');
+const { GetStudent, SearchStudent,SearchStudentByClass, PostStudent, PatchStudent, DeleteStudent, AddAttendance,PromoteStudent } = require('../Controllers/StudentsController');
 const router = new express.Router();
-const { validateToken } = require('../Controllers/Jwt')
+const { validateToken } = require('../Controllers/Jwt');
+const { validate } = require('../Models/FileAttachments');
 // const multer = require('multer');
 
 // Set up Multer for handling file uploads
@@ -15,6 +16,7 @@ router.route("/").post(validateToken, PostStudent);
 router.route("/:id/attendance").post(validateToken, AddAttendance);
 router.route("/:id").patch(validateToken, PatchStudent);
 router.route("/:id").delete(validateToken, DeleteStudent);
+router.route("/promote/:id").post(validateToken,PromoteStudent);
 
 
 module.exports = router;
